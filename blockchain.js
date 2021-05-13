a70f5e3fbc7bd3ba5f1e45caab7aed029a4957a7
Mandy <mandychan.dev@gmail.com>
2021-05-13 08:15:46 +0700

    Update Blockchain model

--- blockchain.js
+++ blockchain.js
@@ -1,14 +1,24 @@
 const Block = require("./block");
 const CryptoJS = require("crypto-js");
+// const BLOCK_GENERATION_INTERVAL = 10;
+// const DIFFICULTY_ADJUSTMENT_INTERVAL = 2016;
 
 class Blockchain {
-  constructor(options) {
+  constructor() {
     this.blockchain = [Block.getGenesisBlock];
-    this.difficulty = 4;
+    // this.difficulty = 4;
   }
 
   get() {
     return this.blockchain;
+  }
+
+  set(chain) {
+    try {
+      this.shouldReplaceChain(chain);
+    } catch (e) {
+      throw e;
+    }
   }
 
   // Ham GET lay ra block cuoi cung
@@ -16,10 +26,25 @@ class Blockchain {
     return this.blockchain[this.blockchain.length - 1];
   }
 
+  // Ham tinh toan difficulty dua tren index cua chinh no
+  getDifficulty(index) {
+    return Math.round(index / 50) + 3;
+  }
+  // getDifficulty(aBlockchain) {
+  //   const lastestBlock = aBlockchain[aBlockchain.length - 1]
+  //   if (lastestBlock.index % DIFFICULTY_ADJUSTMENT_INTERVAL === 0 && lastestBlock.index !== 0) {
+  //     // dieu chinh do kho
+  //     return getAdjustedDifficulty(lastestBlock, aBlockchain)
+  //   } else {
+  //     // mac dinh
+  //     return lastestBlock.index;
+  //   }
+  // }
+
   // Ham tinh toan hash dua tren du lieu cua chinh no
-  calculateHash(index, previousHash, timestamp, data, nonce) {
+  calculateHash(index, previousHash, timestamp, transactions, nonce) {
     return CryptoJS.SHA256(
-      index + previousHash + timestamp + data + nonce
+      index + previousHash + timestamp + JSON.stringify(transactions) + nonce
     ).toString();
   }
 
@@ -29,25 +54,26 @@ class Blockchain {
       block.index,
       block.previousHash,
       block.timestamp,
-      block.data,
+      block.transactions,
       block.nonce
     );
   }
 
   // Ham tao ra block moi dua tren thong tin cua block cuoi cung
-  generateNextBlock(blockData) {
+  generateNextBlock(transactions) {
     const previousBlock = this.latestBlock;
     const nextIndex = previousBlock.index + 1;
     const nextTimestamp = new Date().getTime() / 1000;
     let nonce = 0;
     let nextHash = "";
-    while (!this.hashMatchesDifficulty(nextHash)) {
+    // hashcash proof-of-work: loop until nonce yields hash that passes difficulty.
+    while (!this.hashMatchesDifficulty(nextHash, nextIndex)) {
       nonce = nonce + 1;
       nextHash = this.calculateHash(
         nextIndex,
         previousBlock.hash,
         nextTimestamp,
-        blockData,
+        transactions,
         nonce
       );
     }
@@ -55,7 +81,7 @@ class Blockchain {
       nextIndex,
       previousBlock.hash,
       nextTimestamp,
-      blockData,
+      transactions,
       nextHash,
       nonce
     );
@@ -64,13 +90,17 @@ class Blockchain {
   }
 
   // Ham ktra 1 gia tri hash co du do kho hay khong
-  hashMatchesDifficulty(hash) {
+  hashMatchesDifficulty(hash, index) {
+    // const hashInBinary = hexToBinary(hash);
+    // cons requiredPrefix = "0".repeat(this.getDifficutt(index));
+    // // ktra co bat dau bang n so 0 hay khong
+    // return hashInBinary.startWith(requiredPrefix);
     for (var i = 0, b = hash.length; i < b; i++) {
       if (hash[i] !== "0") {
         break;
       }
     }
-    return i === this.difficulty;
+    return i >= this.getDifficulty(index);
   }
 
   // Ham ktra tinh toan ven cua 1 block
@@ -87,7 +117,10 @@ class Blockchain {
       console.log(`Invalid hash: ${blockHash} ${newBlock.hash}`);
       return false;
     } else if (
-      !this.hashMatchesDifficulty(this.calculateHashForBlock(newBlock))
+      !this.hashMatchesDifficulty(
+        this.calculateHashForBlock(newBlock),
+        newBlock.index
+      )
     ) {
       console.log(
         `Invalid hash does not meet difficulty requirements: ${this.calculateHashForBlock(
@@ -99,8 +132,9 @@ class Blockchain {
     return true;
   }
 
-  // Ham thay the phuc vu viec dong bo giua cac node voi nhau
-  replaceChain(newBlocks) {
+  // Ham ktra khi 1 nut (node) gap 1 khoi co chi so lon hon khoi hien tai
+  // => thay the phuc vu viec dong bo giua cac node voi nhau
+  shouldReplaceChain(newBlocks) {
     if (!this.isValidChain(newBlocks)) {
       console.log(
         "Replacement chain is not valid. Won't replace existing blockchain."
@@ -115,6 +149,13 @@ class Blockchain {
       return null;
     }
 
+    if (!this.isEqualGenesis(newBlocks)) {
+      console.log(
+        `Genesis ${blockchainToValidate[0]} is different from current genesis ${this.blockchain[0]}`
+      );
+      return null;
+    }
+
     console.log(
       "Received blockchain is valid. Replacing current blockchain with received blockchain"
     );
@@ -124,11 +165,25 @@ class Blockchain {
           json.index,
           json.previousHash,
           json.timestamp,
-          json.data,
+          json.transactions,
           json.hash,
           json.nonce
         )
     );
+  }
+
+  // Ham ktra 2 khoi co tuong dong ve block dau tien hay khong
+  isEqualGenesis(blockchainToValidate) {
+    if (
+      JSON.stringify(blockchainToValidate[0]) !==
+      JSON.stringify(this.blockchain[0])
+    ) {
+      console.log(
+        `Genesis ${blockchainToValidate[0]} is different from current genesis ${this.blockchain[0]}`
+      );
+      return false;
+    }
+    return true;
   }
 
   // Ham ktra toan bo blockchain co hop le hay khong
